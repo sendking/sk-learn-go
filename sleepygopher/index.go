7ebc7ce01e49381d7e0c03988a3c6050893b1528
lisk <lisk@mxchip.com>
2021-01-29 15:56:42 +0800

    feat: lesson30

---
+++ sleepygopher/index.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go sleepyGopher()
+	time.Sleep(5 * time.Second)
+}
+func sleepyGopher() {
+	time.Sleep(3 * time.Second)
+	fmt.Println("... snore ...")
+}
---
+++ sleepygophers/index.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 5; i++ {
+		go sleepyGopher(i)
+	}
+	time.Sleep(4 * time.Second)
+}
+func sleepyGopher(i int) {
+	time.Sleep(3 * time.Second)
+	fmt.Println("... snore ...", i)
+}
